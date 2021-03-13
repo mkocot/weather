ffbe30dee0c0f4678aa5a6579973c7f4f914e0b1
mkocot <kociolek.farfocle+github@gmail.com>
2021-03-13 19:05:51 +0000

    Remove unused parsts

--- weather/weather.ino
+++ weather/weather.ino
@@ -1,13 +1,7 @@
 #include <ESP8266WiFi.h> /* wifi */
 #include <Adafruit_BME280.h> /* include Adafruit library for BMP280 sensor */
 #include <WiFiUdp.h> /* UDP */
-#include <time.h>
 #include "config.h"
-
-#define TIME_ZONE 0
-#define TIME_DST 0
-#define USE_TIME 0
-#define SECS_YR_2000  (946684800L) // the time at the start of y2k
 
 Adafruit_BME280  bme;
 // TODO: Put correct value
@@ -68,7 +62,6 @@ void setupBME280() {
 
   // For more details on the following scenarious, see chapter
   // 3.5 "Recommended modes of operation" in the datasheet
-#if 1
   // weather monitoring
   Serial.println("-- Weather Station Scenario --");
   Serial.println("forced mode, 1x temperature / 1x humidity / 1x pressure oversampling,");
@@ -83,86 +76,6 @@ void setupBME280() {
   delayTime = 60000; // in milliseconds
   // change to 5mins
   delayTime = 300000;
-#endif
-
-#if 0
-  // humidity sensing
-  Serial.println("-- Humidity Sensing Scenario --");
-  Serial.println("forced mode, 1x temperature / 1x humidity / 0x pressure oversampling");
-  Serial.println("= pressure off, filter off");
-  bme.setSampling(Adafruit_BME280::MODE_FORCED,
-                  Adafruit_BME280::SAMPLING_X1,   // temperature
-                  Adafruit_BME280::SAMPLING_NONE, // pressure
-                  Adafruit_BME280::SAMPLING_X1,   // humidity
-                  Adafruit_BME280::FILTER_OFF );
-
-  // suggested rate is 1Hz (1s)
-  delayTime = 1000;  // in milliseconds
-#endif
-
-#if 0
-  // indoor navigation
-  Serial.println("-- Indoor Navigation Scenario --");
-  Serial.println("normal mode, 16x pressure / 2x temperature / 1x humidity oversampling,");
-  Serial.println("0.5ms standby period, filter 16x");
-  bme.setSampling(Adafruit_BME280::MODE_NORMAL,
-                  Adafruit_BME280::SAMPLING_X2,  // temperature
-                  Adafruit_BME280::SAMPLING_X16, // pressure
-                  Adafruit_BME280::SAMPLING_X1,  // humidity
-                  Adafruit_BME280::FILTER_X16,
-                  Adafruit_BME280::STANDBY_MS_0_5 );
-
-  // suggested rate is 25Hz
-  // 1 + (2 * T_ovs) + (2 * P_ovs + 0.5) + (2 * H_ovs + 0.5)
-  // T_ovs = 2
-  // P_ovs = 16
-  // H_ovs = 1
-  // = 40ms (25Hz)
-  // with standby time that should really be 24.16913... Hz
-  delayTime = 41;
-#endif
-
-#if 0
-  // gaming
-  Serial.println("-- Gaming Scenario --");
-  Serial.println("normal mode, 4x pressure / 1x temperature / 0x humidity oversampling,");
-  Serial.println("= humidity off, 0.5ms standby period, filter 16x");
-  bme.setSampling(Adafruit_BME280::MODE_NORMAL,
-                  Adafruit_BME280::SAMPLING_X1,   // temperature
-                  Adafruit_BME280::SAMPLING_X4,   // pressure
-                  Adafruit_BME280::SAMPLING_NONE, // humidity
-                  Adafruit_BME280::FILTER_X16,
-                  Adafruit_BME280::STANDBY_MS_0_5 );
-
-  // Suggested rate is 83Hz
-  // 1 + (2 * T_ovs) + (2 * P_ovs + 0.5)
-  // T_ovs = 1
-  // P_ovs = 4
-  // = 11.5ms + 0.5ms standby
-  delayTime = 12;
-#endif
-
-}
-
-void setupTime() {
-  // Looks like time is not retained after deep-sleep
-  time_t now = time(nullptr);
-  Serial.print("time: ");
-  Serial.println(now);
-  
-  if (now > SECS_YR_2000) {
-    Serial.println("Time already set");
-    return;
-  }
-  Serial.println("configure time");
-  configTime(TIME_ZONE, TIME_DST, "pool.ntp.org");
-
-  while (now < SECS_YR_2000) {
-    delay(300);
-    now = time(nullptr);
-    blink();
-  }
-  Serial.println("time configured");
 }
 
 void setup() {
@@ -176,9 +89,6 @@ void setup() {
 
   setupBME280();
   setupWiFi();
-  #if USE_TIME
-  setupTime();
-  #endif
   digitalWrite(LED_BUILTIN, HIGH); /* disable LED */
 
   /* clear packet */
@@ -224,10 +134,6 @@ void sendValues() {
     byte bval[4];
   } f2bytes;
 
-  struct timeval tv = {0, 0};
-  gettimeofday(&tv, NULL);
-
-
   int idx = HEADER_SIZE;
   Udp.beginPacket(UDP_ADDR, UDP_PORT);
 
@@ -257,12 +163,7 @@ void sendValues() {
   replyPacket[idx++] = f2bytes.bval[2];
   replyPacket[idx++] = f2bytes.bval[3];
     
-//  replyPacket[idx++] = 0x04; /* time */
-//  f2bytes.ival = tv.tv_sec - SECS_YR_2000;
-//  replyPacket[idx++] = f2bytes.bval[0];
-//  replyPacket[idx++] = f2bytes.bval[1];
-//  replyPacket[idx++] = f2bytes.bval[2];
-//  replyPacket[idx++] = f2bytes.bval[3];
+  /* 0x04 time (deprecated) */
 
   replyPacket[idx++] = 0x05; /* voltage */
   f2bytes.uval = inVolt;
