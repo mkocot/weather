d317122960e6c2ca972e588d0f8f301e53643703
Marcin Kocot <mk@foo.bz>
2025-09-24 23:31:48 +0200

    Support for multiple bind points

--- weather_backend/weather.py
+++ weather_backend/weather.py
@@ -24,6 +24,7 @@ from config import load_config
 from protocol import (HumiditySensor, PressureSensor, SoilMoistureSensor,
                       TempSensor, VOCSensor, VoltSensor, WindSensor, THPCompound, WindSpeedDirection)
 
+DEBUG = False
 USE_ZMQ = True
 
 if USE_ZMQ:
@@ -49,7 +50,7 @@ logging.basicConfig(format='%(asctime)s %(message)s')
 
 cfg = load_config('./config.toml')
 # check if we have storage DIR
-storage_path = cfg['storage']['path']
+storage_path = str(cfg['storage']['path'])
 if not exists(storage_path):
     mkdir(storage_path)
 if not isdir(storage_path):
@@ -57,117 +58,6 @@ if not isdir(storage_path):
     exit(1)
 
 DB = fetch.SQLiteDB(storage_path, ro=True)
-
-
-class ScreenInfo:
-    def __init__(self):
-        self.deadline = 0.0
-        self.state = 0
-        self.sensor_idx = 0
-        self.time_to_swith_state = 0
-        self.addr = 'localhost'
-
-
-class ScreenThread:
-    _lock = asyncio.Lock()
-    _screens = {}
-    _db = DB
-    _keep_looping = True
-
-    def __init__(self, sock):
-        self.sock = sock
-
-    async def run(self):
-        while self._keep_looping:
-            await self._loop()
-            await asyncio.sleep(1)
-
-    async def _loop(self):
-        now = time.time()
-        async with self._lock:
-            devices = list(self._screens.keys())
-            for d in devices:
-                if self._screens[d].deadline < now:
-                    del self._screens[d]
-            devices = self._screens.copy()
-
-        # generate images
-        for did, d in devices.items():
-            if d.time_to_swith_state > now:
-                continue
-            d.time_to_swith_state = now + 5
-            sensors = cfg['device'][did].get('screen')
-            if not sensors:
-                continue
-            sensors_names = list(sensors.keys())
-            # TODO(m): why empty
-            if not sensors_names:
-                continue
-            if d.sensor_idx >= len(sensors_names):
-                d.sensor_idx = 0
-            sensor_id = sensors_names[d.sensor_idx]
-            selected_sensor = sensors[sensor_id]
-            name = cfg['device'][sensor_id]['name']
-            # get data from last 8 hours
-            raw_data = self._db.fetch(sensor_id, start=8 * 60 * 60)
-            # states
-            # 0 -> overview
-            # 1 -> temperature graph
-            # 2 -> humidity graph
-            # 3 -> pressure graph
-            # 4 -> volate graph
-            # then repeat for another sensor
-
-            def _filter(data):
-                return [x for x in data if x is not None]
-            temp = _filter(raw_data['temp'])
-            hum = _filter(raw_data['hum'])
-            press = _filter(raw_data['pres'])
-            volt = _filter(raw_data['volt'])
-            if d.state == 0:
-                image = draw.draw_overview(name, temp=temp[-1], hum=hum[-1],
-                                           press=press[-1], volt=volt[-1],
-                                           time=raw_data['time'][len(temp) - 1])
-            elif d.state == 1:
-                image = draw.draw_graph('Temp', name, temp)
-            elif d.state == 2:
-                image = draw.draw_graph('Hum', name, hum)
-            elif d.state == 3:
-                image = draw.draw_graph('Pres', name, press)
-            elif d.state == 4:
-                image = draw.draw_graph('Volt', name, volt)
-            else:
-                image = draw.draw_overview(name)
-            df = protocol.DataFrame()
-            df.device_id = did
-            df.message_to_broker = False
-            df.modules = [
-                protocol.ImagePush(128, 32, image)
-            ]
-            data = protocol.serialize(df)
-            self.sock.sendto(data, (d.addr, 9696))
-            d.state += 1
-            if d.state > 4:
-                d.sensor_idx += 1
-                d.state = 0
-
-    async def add_screen(self, id, addr, timeout=120):
-        # TODO: sensor 1 and sensor 2 is swapend on scrren vs web
-        # TODO: async with is without timeout  (2 secs)
-        async with self._lock:
-            screen = self._screens.get(id)
-            if not screen:
-                screen = ScreenInfo()
-                self._screens[id] = screen
-            screen.deadline = time.time() + timeout
-            screen.addr = addr[0]
-
-
-DEBUG = False
-
-# st = ScreenThread()
-# st.start()
-# st.add_screen("e8db849381ec", ("10.0.0.28", 0))
 
 
 class WOutEncoder(json.JSONEncoder):
@@ -180,8 +70,7 @@ class WOutEncoder(json.JSONEncoder):
 class WeatherProcessor:
     def __init__(self, cfg):
         self.cfg = cfg
-        self.sock = self._prepare_socket()
-        self.st = ScreenThread(self.sock)
+        self.sock = list(self._prepare_socket())
         self.zmq = None
         # hold active devices, prune if timeout is larger than 30min
         self.sensors = {}
@@ -191,20 +80,24 @@ class WeatherProcessor:
     def _prepare_socket(self):
         MCAST_GRP = '239.87.84.82'  # (239.W.T.R)
 
-        host, port = self.cfg['bind']['address'].split(':')
-        # host = ""
-        sock = socket.socket(
-            socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
-        sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-        # sock.setsockopt(socket.SOL_SOCKET, socket.SO_BINDTODEVICE, b"enp6s0")
-        # host = ''
-        sock.bind((host, int(port)))
-        mreq = struct.pack('4sl', socket.inet_aton(
-            MCAST_GRP), socket.INADDR_ANY)
-        sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
-        return sock
+        for bind in self.cfg['bind']:
+            if 'address' not in bind:
+                continue
+
+            host, port = bind['address'].split(':')
+            # host = ""
+            sock = socket.socket(
+                socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+            sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+            sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+            # sock.setsockopt(socket.SOL_SOCKET, socket.SO_BINDTODEVICE, b"enp6s0")
+            # host = ''
+            sock.bind((host, int(port)))
+            mreq = struct.pack('4sl', socket.inet_aton(
+                MCAST_GRP), socket.INADDR_ANY)
+            sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+            yield sock
 
     if USE_ZMQ:
         def _prepare_zmq(self):
@@ -220,10 +113,9 @@ class WeatherProcessor:
     async def run(self):
         # 1) connect to broker
         # udp_task = file_receiver(self) 
-        udp_task = udp_receiver(self)
-        uart_task = uart_receiver(self)
-        screen_task = screen_sender(self)
-        tasks = [udp_task, uart_task, screen_task]
+        udp_task = list(udp_receiver(self))
+        uart_task = list(uart_receiver(self))
+        tasks  = udp_task + uart_task
         x = await asyncio.wait(
             [asyncio.create_task(coro) for coro in tasks],
             return_when=asyncio.FIRST_COMPLETED
@@ -501,17 +393,17 @@ class WeaterServerProtocol(asyncio.DatagramProtocol):
         self.emergency_stop.set_exception(exc)
 
 
-async def udp_receiver(patocol):
-    sock = patocol.sock
+def udp_receiver(patocol):
     loop = asyncio.get_running_loop()
-
-    emergency_stop = loop.create_future()
-
-    transport, protocol = await loop.create_datagram_endpoint(lambda: WeaterServerProtocol(emergency_stop, patocol), sock=sock)
-    await protocol.emergency_stop
-    transport.close()
-    sock.close()
-    print('boom')
+    for sock in patocol.sock:
+        async def x():
+            emergency_stop = loop.create_future()
+            transport, protocol = await loop.create_datagram_endpoint(lambda: WeaterServerProtocol(emergency_stop, patocol), sock=sock)
+            await protocol.emergency_stop
+            transport.close()
+            sock.close()
+            print('boom')
+        yield x()
 
 async def file_receiver(patocol):
     loop = asyncio.get_running_loop()
@@ -527,30 +419,29 @@ async def file_receiver(patocol):
 
 
 
-async def uart_receiver(patocol):
-    serial_dev = cfg['bind'].get('serial_dev')
-    if not serial_dev:
-        while True:
-            await asyncio.sleep(100000)
-        return
-    serial_baud = int(cfg['bind'].get('serial_baud', 115200))
-    loop = asyncio.get_running_loop()
-    emergency_stop = loop.create_future()
-    serial_protocol = cfg['bind'].get('serial_protocol', 'HEX')
-    if serial_protocol == 'HEX':
-        proto = WeaterServerUARTProtocol(emergency_stop, patocol)
-    else:
-        proto = WeatherServerHC12UARTProtocol(emergency_stop, patocol)
-    # soo there is some special options that should be enabled to
-    # make serial happy?
-    coro = serial_asyncio.create_serial_connection(
-        loop, lambda: proto, serial_dev, baudrate=serial_baud)
-    transport, protocol = await coro
-    await protocol.emergency_stop
-
-
-async def screen_sender(patocol: WeatherProcessor):
-    await patocol.st.run()
+def uart_receiver(patocol):
+    for bind in cfg['bind']:
+        if 'serial_dev' not in bind:
+            continue
+        serial_dev = bind.get('serial_dev')
+        if not serial_dev:
+            continue
+        serial_baud = int(bind.get('serial_baud', 115200))
+        loop = asyncio.get_running_loop()
+        emergency_stop = loop.create_future()
+        serial_protocol = bind.get('serial_protocol', 'HEX')
+        if serial_protocol == 'HEX':
+            proto = WeaterServerUARTProtocol(emergency_stop, patocol)
+        else:
+            proto = WeatherServerHC12UARTProtocol(emergency_stop, patocol)
+        # soo there is some special options that should be enabled to
+        # make serial happy?
+        coro = serial_asyncio.create_serial_connection(
+            loop, lambda: proto, serial_dev, baudrate=serial_baud)
+        async def x():
+            transport, protocol = await coro
+            await protocol.emergency_stop
+        yield x()
 
 
 async def zmq_notifier():
@@ -565,9 +456,6 @@ async def main():
     # create tasks
     # asyncio.seri
     processor = WeatherProcessor(cfg)
-
-    NEW_DATA = b'W\x02\x02\x01\x12\x00.\x03.\x01-\x01.\x01\x00-\x01-'
-    await processor.process(NEW_DATA)
 
     error = await processor.run()
     print('should not be here', error)
